5c46de8cb62c66027313dece389630d912196baa
dimitur.trz <dimitar.trz@gmail.com>
2014-04-01 20:19:39 +0300

    1.1-three files - attack and card

--- card.h
+++ card.h
@@ -5,16 +5,4 @@ typedef struct {
 	int force;
 	int cost;
 } card_t;
-int attack(card_t *attacker, card_t *defender){
-	defender->life-=attacker->force;
-	attacker->life-=defender->force;
-	if(defender->life<0 && attacker->life>0){
-		return 1;
-	}
-	if(defender->life>0 && attacker->life<0){
-		return 2;	
-	}
-	if(defender->life>0 && attacker->life>0){
-		return 3;
-	}
-}
+
